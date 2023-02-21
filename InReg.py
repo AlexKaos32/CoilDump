2a616ea94bfdaaedd3930bdba88ff6df54f3762b
Alex <59406285+AlexKaos32@users.noreply.github.com>
2023-02-21 13:44:49 -0600

    Add files via upload

---
+++ InReg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pymodbus.client import ModbusTcpClient as ModbusClient
+from pymodbus.exceptions import ConnectionException
+
+# This is the ip of the remote machine running modbus, you pass this to the tool at runtime
+ip = sys.argv[1]
+
+# Passes IP and port number to ModbusTcpClient/ModbusClient, sets that to a var named client
+client = ModbusClient(ip, port=502)
+
+# Connects to remote machine on port 502
+client.connect()
+# while statement is going to run until stopped with ctrl+c or an error is reached
+while True:
+    request = client.read_input_registers(1, 16)
+    if request.isError():
+           print('Modbus Error:', request)
+    else:
+        result = request.registers
+        print(result)
+# time.sleep is providing a waiting period before checking again
+    time.sleep(1)
